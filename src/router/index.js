5e73fe0cd695413a172b4483ab84bfc5a97ee2e9
Ryan Frazier <frazierr2@gmail.com>
2019-05-13 17:06:59 -0400

    trying to get route to work again

--- src/router/index.js
+++ src/router/index.js
@@ -10,7 +10,7 @@ export default new Router({
     mode: 'history',
     routes: [
         {
-            path: '/',
+            path: '/booj-books-app',
             name: 'home',
             component: HomeComponent
         },
