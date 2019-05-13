758c8e9388b3483f9e7e0d8b2acba2b74f8dc1c0
Ryan Frazier <frazierr2@gmail.com>
2019-05-13 17:01:32 -0400

    update to deploy

--- deploy.sh
+++ deploy.sh
@@ -17,9 +17,9 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io
-git push -f git@github.com:frazierr2/frazierr2.github.io.git master
+# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:frazierr2/booj-books-app.git master:gh-pages
+git push -f git@github.com:frazierr2/booj-books-app.git master:gh-pages
 
 cd -
